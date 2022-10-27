6629ae9d649666c187a915ba4ed9aafe880476dd
salim <jimsky699@gmail.com>
2022-10-27 08:50:10 +0100

    make fake subscriptions

--- helpers/fakeData.controler.js
+++ helpers/fakeData.controler.js
@@ -2,6 +2,7 @@ const Meditation = require("../models/meditation.model");
 const User = require("../models/user.model");
 const { faker } = require("@faker-js/faker");
 const dayjs = require("dayjs");
+const Subscription = require("../models/subscription.model");
 
 module.exports.fakeMeditaton = (_, res) => {
     const format = (date) => dayjs(date).format("MM-DD-YYYY")
@@ -37,6 +38,32 @@ module.exports.fakeUser = (_, res) => {
                 phone: faker.phone.number('+2376########'),
             })
             user.save()
+        }
+    } catch (error) {
+        res.status(400).json({
+            error: true,
+            message: "erreur lors de la génération des données",
+            data: []
+        })
+        return;
+    }
+    res.status(200).json({
+        error: false,
+        message: "donnée généré avec succès !",
+        data: []
+    })
+}
+
+module.exports.fakeSubscription = (_, res) => {
+    const format = (date) => dayjs(date).format("MM-DD-YYYY")
+    try {
+        for (let i = 0; i < 10; i++) {
+            let meditation = new Subscription({
+                userId: faker.database.mongodbObjectId(),
+                startDate: format(faker.date.between('2022-01-01', '2023-01-01')),
+                endDate: format(faker.date.between('2022-02-02', '2023-02-02')),
+            })
+            meditation.save()
         }
     } catch (error) {
         res.status(400).json({
--- helpers/fakeData.route.js
+++ helpers/fakeData.route.js
@@ -4,5 +4,6 @@ const fakeData = require('./fakeData.controler')
 
 router.post('/meditations', fakeData.fakeMeditaton)
 router.post('/users', fakeData.fakeUser)
+router.post('/subscriptions', fakeData.fakeSubscription)
 
 module.exports = router;
